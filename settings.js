e2364344984a96d85262d4dcd14dac057812e0dc
𝐄𝐦𝐩𝐫𝐞𝐬𝐬†𝐒𝐮𝐤𝐮𝐧𝐚 <samniaqueen@gmail.com>
2025-01-09 13:53:50 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,9 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "𝕰𝖒𝖕𝖗𝖊𝖘𝖘 𝕾𝖚𝖐𝖚𝖓𝖆.
+"
 
-global.banknumber = "8133729715"
+global.banknumber = "50940757846"
 
-global.bankowner = "FADARE"
+global.bankowner = "PATRON"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
